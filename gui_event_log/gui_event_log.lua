82b3da0df424ada85aa758a158bdef439cd824bb
Wybren Koelmans <wybrenkoelmans@gmail.com>
2025-09-27 17:03:27 +0200

    wip

--- gui_event_log/gui_event_log.lua
+++ gui_event_log/gui_event_log.lua
@@ -5,7 +5,7 @@ end
 function widget:GetInfo()
 	return {
 		name      = "Event log",
-		desc      = "A list of important events. Most events are interactive by clicking the item.",
+		desc      = ".",
 		author    = "uBdead",
 		date      = "Sept 2025",
 		license   = "GPL V2 or later",
@@ -26,15 +26,11 @@ function widget:Initialize()
 	-- use the DataModel handle to set values
 	-- only keys declared at the DataModel's creation can be used
 	dm_handle = widget.rmlContext:OpenDataModel("gui_event_log", {
+        example = "Hello World",
 		events = {
-			{ time = "12:01", type = "alert", message = "Nuclear missile ready" },
-			{ time = "12:03", type = "warning", message = "Commander under attack" },
-			{ time = "12:05", type = "info", message = "Metal extractor built" },
-			{ time = "12:07", type = "alert", message = "Enemy superweapon detected" },
-			{ time = "12:10", type = "info", message = "Ally has shared resources" },
-			{ time = "12:12", type = "warning", message = "Base perimeter breached" },
-			{ time = "12:15", type = "info", message = "Unit promoted: Veteran" },
-			{ time = "12:18", type = "alert", message = "Commander destroyed" },
+			{ time = "12:18", type = "alert", message = "Example 1" },
+			{ time = "12:18", type = "alert", message = "Example 2" },
+			{ time = "12:18", type = "alert", message = "Example 3" },
 		},
 	});
 
